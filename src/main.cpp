4a9bdcb7260c15f244131c98807baeb33d78c53e
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-04-26 23:52:33 +0200

    Add sdl2 (#1)
    Add SDL2 and create a basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,25 @@
+#include <SDL2/SDL.h>
+
 #include <stdio.h>
 
-int main() {
+int main(int argc, char* args[]) {
 	printf("Hello Game Engine 2024!\n");
+
+	if (SDL_Init(SDL_INIT_VIDEO)) {
+		printf("SDL_Init failed with error: %s\n", SDL_GetError());
+	}
+
+	SDL_Window* window = SDL_CreateWindow(
+		"Game Engine 2024",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		680,
+		480,
+		0
+	);
+
+	SDL_Delay(5000);
+
+	SDL_Quit();
+	return 0;
 }
---
+++ todo.md
@@ -0,0 +1,10 @@
+## TODO
+- Add OpenGL
+- Add ImGui
+- Add Google Test
+
+## Doing
+- Create SDL2 window
+
+## Done
+- Add clang format
