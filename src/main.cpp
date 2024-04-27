3a2aa60b3f1e59801bc10aaabb1774e9969f91ec
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-04-27 21:57:00 +0200

    Read DLL timestamp

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@
 
 using EngineUpdateFn = void(engine::EngineState*);
 
+constexpr bool PRINT_DLL_TIMESTAMPS = false;
+
 const char* vertex_shader_src =
 	"#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
@@ -42,6 +44,21 @@ void GLAPIENTRY on_opengl_error(
 	const void* /*userParam*/
 ) {
 	fprintf(stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n", (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""), type, severity, message);
+}
+
+void print_last_winapi_error() {
+	DWORD err_code = GetLastError();
+	char* err_msg;
+	if (!FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, err_code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // default language
+					   (LPTSTR)&err_msg,
+					   0,
+					   NULL))
+		return;
+
+	static char buffer[1024];
+	_snprintf_s(buffer, sizeof(buffer), "%s", err_msg);
+	fprintf(stderr, "%s", buffer);
+	LocalFree(err_msg);
 }
 
 int main(int /*argc*/, char** /*args*/) {
@@ -183,15 +200,45 @@ int main(int /*argc*/, char** /*args*/) {
 	}
 
 	/* Load engine DLL */
+	// 1. make a copy of DLL (so original can be rebuilt)
+	// 2. load DLL copy
+	// 3. save DLL copy timestamp
+	// 4. while running, if DLL timestamp newer than DLL copy timestamp, goto 1
+	FILETIME last_dll_write = {};
 	std::function<EngineUpdateFn> engine_update = [](engine::EngineState*) {};
 	{
+		/* Load DLL */
 		const char* dll_name = "GameEngine2024.dll";
-		HMODULE engine_dll = LoadLibraryA(dll_name);
+		HMODULE engine_dll = LoadLibrary(dll_name);
 		if (!engine_dll) {
 			fprintf(stderr, "error: LoadLibraryA(\"%s\") returned null. Does the DLL exist?\n", dll_name);
 			exit(1);
 		}
 
+		// DEBUG print out dll_name full path
+		char dll_full_path[MAX_PATH];
+		if (GetModuleFileName(engine_dll, dll_full_path, sizeof(dll_full_path)) == 0) {
+			fprintf(stderr, "error: GetModuleFileName(\"%s\") failed:", dll_name);
+			print_last_winapi_error();
+		}
+
+		/* Read last write timestamp */
+		{
+			WIN32_FILE_ATTRIBUTE_DATA data;
+			if (GetFileAttributesEx(dll_full_path, GetFileExInfoStandard, &data)) {
+				last_dll_write = data.ftLastWriteTime;
+				if (PRINT_DLL_TIMESTAMPS) {
+					SYSTEMTIME st;
+					FileTimeToSystemTime(&last_dll_write, &st);
+					printf("Last file write to %s: %04d-%02d-%02d %02d:%02d:%02d\n", dll_name, st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
+				}
+			} else {
+				fprintf(stderr, "error: GetFileAttributesEx(\"dll_full_path\") failed with: ");
+				print_last_winapi_error();
+			}
+		}
+
+		/* Read functions */
 		{
 			const char* fn_name = "engine_update";
 			EngineUpdateFn* fn = (EngineUpdateFn*)(GetProcAddress(engine_dll, fn_name));
