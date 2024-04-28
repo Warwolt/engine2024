b66cceefa25e46565fb475ac941ae36ffe546a99
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-04-28 23:17:44 +0200

    Format

--- src/main.cpp
+++ src/main.cpp
@@ -78,11 +78,17 @@ void GLAPIENTRY on_opengl_error(
 void print_last_winapi_error() {
 	DWORD err_code = GetLastError();
 	char* err_msg;
-	if (!FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, err_code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // default language
-					   (LPTSTR)&err_msg,
-					   0,
-					   NULL))
+	if (!FormatMessage(
+			FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+			NULL,
+			err_code,
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // default language
+			(LPTSTR)&err_msg,
+			0,
+			NULL
+		)) {
 		return;
+	}
 
 	static char buffer[1024];
 	_snprintf_s(buffer, sizeof(buffer), "%s", err_msg);
