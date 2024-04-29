3cef2c7d90c31ddf19ffc643b3f28d8d9cd54a2d
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-04-29 03:11:10 +0200

    Use LOG_XX macros instead of printf

--- src/platform/logging.h
+++ src/platform/logging.h
@@ -4,7 +4,15 @@
 #define PLOG_CAPTURE_FILE
 #include <plog/Log.h>
 
+// define printf style logging macros
+#define LOG_VERBOSE(...) PLOG_VERBOSE.printf(__VA_ARGS__)
+#define LOG_DEBUG(...) PLOG_DEBUG.printf(__VA_ARGS__)
 #define LOG_INFO(...) PLOG_INFO.printf(__VA_ARGS__)
+#define LOG_WARNING(...) PLOG_WARNING.printf(__VA_ARGS__)
+#define LOG_ERROR(...) PLOG_ERROR.printf(__VA_ARGS__)
+#define LOG_FATAL(...) PLOG_FATAL.printf(__VA_ARGS__)
+#define LOG_NONE(...) PLOG_NONE.printf(__VA_ARGS__)
+#define LOG(level, ...) PLOG(level).printf(__VA_ARGS__)
 
 namespace platform {
 
