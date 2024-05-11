551152b3eb9829390735892547927228efd77b1f
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-05-11 17:48:13 +0200

    doc

--- src/platform/renderer.h
+++ src/platform/renderer.h
@@ -40,11 +40,21 @@ namespace platform {
 		GLuint vbo;
 	};
 
+	// Origin in upper left corner (e.g. xy-coordinates)
+	// top_left o------------o
+	//          |           |
+	//          |           |
+	//          o-----------o bottom_right
 	struct Rect {
 		glm::vec2 top_left;
 		glm::vec2 bottom_right;
 	};
 
+	// Origin in bottom left corner (e.g. uv-coordinates)
+	//             o-----------o top_right
+	//             |           |
+	//             |           |
+	// bottom_left o-----------o
 	struct FlipRect {
 		glm::vec2 bottom_left;
 		glm::vec2 top_right;
