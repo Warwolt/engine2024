4b2d52e02d2048e3e4c710033d11e0102bea45c6
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-06-03 00:45:03 +0200

    AnimationSystem class

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,52 @@
 #include <vector>
 
 /* Animation Core */
-using AnimationID = int;
+using AnimationKey = std::string;
+struct AnimationID {
+	AnimationKey key;
+	int value = 0;
+};
 struct Animation {
 	AnimationID id;
 	float start;
 	float length;
 	bool repeats;
 };
+
+class AnimationSystem {
+public:
+	const std::vector<Animation>& animations(AnimationKey key);
+
+	AnimationID start_animation(AnimationKey key, float length, float global_time);
+	void stop_animation(AnimationID id);
+
+private:
+	std::unordered_map<AnimationKey, std::vector<Animation>> m_animations;
+};
+
+const std::vector<Animation>& AnimationSystem::animations(AnimationKey key) {
+	return m_animations[key];
+}
+
+AnimationID AnimationSystem::start_animation(AnimationKey key, float length, float global_time) {
+	static int id_value;
+	AnimationID id = {.key = key, .value = ++id_value};
+	m_animations[key].push_back(
+		Animation {
+			.id = id,
+			.start = global_time,
+			.length = length,
+			.repeats = true,
+		}
+	);
+	return id;
+}
+
+void AnimationSystem::stop_animation(AnimationID id) {
+	std::erase_if(m_animations[id.key], [id_value = id.value](const Animation& animation) {
+		return animation.id.value == id_value;
+	});
+}
 
 float local_animation_time(const Animation& animation, float global_time) {
 	return fmodf((global_time - animation.start), animation.length) / animation.length;
@@ -27,25 +66,6 @@ bool animation_is_active(const Animation& animation, float global_time) {
 	}
 }
 
-AnimationID start_animation(std::vector<Animation>* animations, float length, float global_time) {
-	static AnimationID id = 0;
-	animations->push_back(
-		Animation {
-			.id = ++id,
-			.start = global_time,
-			.length = length,
-			.repeats = true,
-		}
-	);
-	return id;
-}
-
-void stop_animation(std::vector<Animation>* animations, AnimationID id) {
-	std::erase_if(*animations, [id](const Animation& animation) {
-		return animation.id == id;
-	});
-}
-
 /* Application */
 std::string loading_text(float t) {
 	if (t < 1.0 / 3.0) {
@@ -58,23 +78,23 @@ std::string loading_text(float t) {
 }
 
 int main() {
-	std::unordered_map<std::string, std::vector<Animation>> animations;
+	AnimationSystem animation_system;
+	AnimationID loading_text_id;
 
-	AnimationID id = 0;
 	for (float global_time = 0; global_time <= 25; global_time++) {
 		/* Trigger animation */
 		if (global_time == 5.0) {
-			id = start_animation(&animations["loading_text"], 6, global_time);
+			loading_text_id = animation_system.start_animation("loading_text", 3, global_time);
 		}
 
 		/* Stop animation */
 		if (global_time == 15.0) {
-			stop_animation(&animations["loading_text"], id);
+			animation_system.stop_animation(loading_text_id);
 		}
 
 		/* Run animation */
 		std::string str;
-		for (const Animation& animation : animations["loading_text"]) {
+		for (const Animation& animation : animation_system.animations("loading_text")) {
 			if (animation_is_active(animation, global_time)) {
 				float local_time = local_animation_time(animation, global_time);
 				str = loading_text(local_time);
