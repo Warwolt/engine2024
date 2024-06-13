653846e97b7810bf3c8ab0668455dff57e8f09ab
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-06-13 22:37:25 +0200

    Add some more tests

--- test/platform/tagged_variant_tests.cpp
+++ test/platform/tagged_variant_tests.cpp
@@ -21,3 +21,24 @@ TEST(TaggedVariantTests, DefaultVariantHasDefaultTag) {
 	TestVariant variant;
 	EXPECT_EQ(variant.tag(), Tag::A);
 }
+
+TEST(TaggedVariantTests, DefaultVariantHasDefaultValue) {
+	TestVariant variant;
+	EXPECT_TRUE(std::holds_alternative<VariantA>(variant));
+}
+
+TEST(TaggedVariantTests, ConstructorAssignsTag) {
+	TestVariant variant = VariantB();
+	EXPECT_EQ(variant.tag(), Tag::B);
+}
+
+TEST(TaggedVariantTests, ConstructorAssignsValue) {
+	TestVariant variant = VariantB();
+	EXPECT_TRUE(std::holds_alternative<VariantB>(variant));
+}
+
+TEST(TaggedVariantTests, AssignmentOperatorAssignsTag) {
+	TestVariant variant = VariantA();
+	variant = VariantB();
+	EXPECT_EQ(variant.tag(), Tag::B);
+}
