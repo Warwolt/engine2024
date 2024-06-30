68e221c12c30193721839e5fd8fa682c061ffc81
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-06-30 02:44:19 +0200

    Update src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,6 @@
 #include <imgui/backends/imgui_impl_opengl3.h>
 #include <imgui/backends/imgui_impl_sdl2.h>
 #include <imgui/imgui.h>
-#include <miniz/miniz.h>
 
 #include <expected>
 #include <filesystem>
