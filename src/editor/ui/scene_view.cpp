975d1583b125666512e9d4a0e1ca4385deed1968
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-08-11 13:24:25 +0200

    Change canvas default size

--- src/editor/ui/scene_view.cpp
+++ src/editor/ui/scene_view.cpp
@@ -7,8 +7,8 @@
 namespace editor {
 
 	void init_scene_view(SceneViewState* scene) {
-		constexpr int canvas_width = 320;
-		constexpr int canvas_height = 320;
+		constexpr int canvas_width = 1600;
+		constexpr int canvas_height = 1200;
 		scene->canvas_size = { canvas_width, canvas_height };
 		scene->scaled_canvas_rect = core::Rect { { 0, 0 }, { canvas_width, canvas_height } };
 		scene->canvas = platform::add_canvas(canvas_width, canvas_height);
