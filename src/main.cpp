e220b29436c3d45ed1a190953ef3caa6397e3eae
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-08-11 12:25:33 +0200

    Add Win32 based color theme

--- src/main.cpp
+++ src/main.cpp
@@ -56,6 +56,81 @@ static void set_normalized_device_coordinate_projection(platform::Renderer* rend
 	renderer->set_projection(shader_program, projection);
 }
 
+static void set_imgui_style_win32_like() {
+	ImGuiStyle& style = ImGui::GetStyle();
+	ImVec4* colors = style.Colors;
+
+	style.FrameBorderSize = 1.0f;
+	style.TabBorderSize = 1.0f;
+	style.DockingSeparatorSize = 1.0f;
+
+	style.WindowRounding = 0.0f;
+	style.ChildRounding = 0.0f;
+	style.FrameRounding = 0.0f;
+	style.PopupRounding = 0.0f;
+	style.ScrollbarRounding = 0.0f;
+	style.GrabRounding = 0.0f;
+	style.TabRounding = 0.0f;
+
+	// clang-format off
+	colors[ImGuiCol_Text]                   = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+	colors[ImGuiCol_TextDisabled]           = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
+	colors[ImGuiCol_WindowBg]               = ImVec4(0.94f, 0.94f, 0.94f, 1.00f);
+	colors[ImGuiCol_ChildBg]                = ImVec4(0.94f, 0.94f, 0.94f, 0.00f);
+	colors[ImGuiCol_PopupBg]                = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+	colors[ImGuiCol_Border]                 = ImVec4(0.42f, 0.42f, 0.42f, 0.35f);
+	colors[ImGuiCol_BorderShadow]           = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+	colors[ImGuiCol_FrameBg]                = ImVec4(1.00f, 1.00f, 1.00f, 0.54f);
+	colors[ImGuiCol_FrameBgHovered]         = ImVec4(0.90f, 0.95f, 0.98f, 1.00f);
+	colors[ImGuiCol_FrameBgActive]          = ImVec4(0.80f, 0.89f, 0.97f, 1.00f);
+	colors[ImGuiCol_TitleBg]                = ImVec4(0.80f, 0.80f, 0.80f, 1.00f);
+	colors[ImGuiCol_TitleBgActive]          = ImVec4(0.80f, 0.80f, 0.80f, 1.00f);
+	colors[ImGuiCol_TitleBgCollapsed]       = ImVec4(0.94f, 0.94f, 0.94f, 1.00f);
+	colors[ImGuiCol_MenuBarBg]              = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+	colors[ImGuiCol_ScrollbarBg]            = ImVec4(0.94f, 0.94f, 0.94f, 0.53f);
+	colors[ImGuiCol_ScrollbarGrab]          = ImVec4(0.80f, 0.80f, 0.80f, 1.00f);
+	colors[ImGuiCol_ScrollbarGrabHovered]   = ImVec4(0.66f, 0.66f, 0.66f, 1.00f);
+	colors[ImGuiCol_ScrollbarGrabActive]    = ImVec4(0.47f, 0.47f, 0.47f, 1.00f);
+	colors[ImGuiCol_CheckMark]              = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+	colors[ImGuiCol_SliderGrab]             = ImVec4(0.80f, 0.80f, 0.80f, 1.00f);
+	colors[ImGuiCol_SliderGrabActive]       = ImVec4(0.47f, 0.47f, 0.47f, 1.00f);
+	colors[ImGuiCol_Button]                 = ImVec4(0.88f, 0.88f, 0.88f, 1.00f);
+	colors[ImGuiCol_ButtonHovered]          = ImVec4(0.90f, 0.95f, 0.98f, 1.00f);
+	colors[ImGuiCol_ButtonActive]           = ImVec4(0.80f, 0.89f, 0.97f, 1.00f);
+	colors[ImGuiCol_Header]                 = ImVec4(0.88f, 0.88f, 0.88f, 1.00f);
+	colors[ImGuiCol_HeaderHovered]          = ImVec4(0.90f, 0.95f, 0.98f, 1.00f);
+	colors[ImGuiCol_HeaderActive]           = ImVec4(0.80f, 0.89f, 0.97f, 1.00f);
+	colors[ImGuiCol_Separator]              = ImVec4(0.43f, 0.43f, 0.50f, 0.50f);
+	colors[ImGuiCol_SeparatorHovered]       = ImVec4(0.10f, 0.40f, 0.75f, 0.78f);
+	colors[ImGuiCol_SeparatorActive]        = ImVec4(0.10f, 0.40f, 0.75f, 1.00f);
+	colors[ImGuiCol_ResizeGrip]             = ImVec4(0.80f, 0.80f, 0.80f, 1.00f);
+	colors[ImGuiCol_ResizeGripHovered]      = ImVec4(0.66f, 0.66f, 0.66f, 1.00f);
+	colors[ImGuiCol_ResizeGripActive]       = ImVec4(0.47f, 0.47f, 0.47f, 1.00f);
+	colors[ImGuiCol_Tab]                    = ImVec4(0.94f, 0.94f, 0.94f, 1.00f);
+	colors[ImGuiCol_TabHovered]             = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+	colors[ImGuiCol_TabActive]              = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+	colors[ImGuiCol_TabUnfocused]           = ImVec4(0.94f, 0.94f, 0.94f, 0.94f);
+	colors[ImGuiCol_TabUnfocusedActive]     = ImVec4(0.94f, 0.94f, 0.94f, 1.00f);
+	colors[ImGuiCol_DockingPreview]         = ImVec4(0.66f, 0.66f, 0.66f, 1.00f);
+	colors[ImGuiCol_DockingEmptyBg]         = ImVec4(0.20f, 0.20f, 0.20f, 1.00f);
+	colors[ImGuiCol_PlotLines]              = ImVec4(0.61f, 0.61f, 0.61f, 1.00f);
+	colors[ImGuiCol_PlotLinesHovered]       = ImVec4(1.00f, 0.43f, 0.35f, 1.00f);
+	colors[ImGuiCol_PlotHistogram]          = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
+	colors[ImGuiCol_PlotHistogramHovered]   = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
+	colors[ImGuiCol_TableHeaderBg]          = ImVec4(0.19f, 0.19f, 0.20f, 1.00f);
+	colors[ImGuiCol_TableBorderStrong]      = ImVec4(0.31f, 0.31f, 0.35f, 1.00f);
+	colors[ImGuiCol_TableBorderLight]       = ImVec4(0.23f, 0.23f, 0.25f, 1.00f);
+	colors[ImGuiCol_TableRowBg]             = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+	colors[ImGuiCol_TableRowBgAlt]          = ImVec4(1.00f, 1.00f, 1.00f, 0.06f);
+	colors[ImGuiCol_TextSelectedBg]         = ImVec4(0.58f, 0.76f, 0.91f, 0.57f);
+	colors[ImGuiCol_DragDropTarget]         = ImVec4(1.00f, 1.00f, 0.00f, 0.90f);
+	colors[ImGuiCol_NavHighlight]           = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
+	colors[ImGuiCol_NavWindowingHighlight]  = ImVec4(1.00f, 1.00f, 1.00f, 0.70f);
+	colors[ImGuiCol_NavWindowingDimBg]      = ImVec4(0.80f, 0.80f, 0.80f, 0.20f);
+	colors[ImGuiCol_ModalWindowDimBg]       = ImVec4(0.80f, 0.80f, 0.80f, 0.35f);
+	// clang-format on
+}
+
 static void init_imgui(SDL_Window* window, SDL_GLContext gl_context) {
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
@@ -64,10 +139,21 @@ static void init_imgui(SDL_Window* window, SDL_GLContext gl_context) {
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable; // Enable window docking
 
-	ImGui::StyleColorsDark();
+	// ImGui::StyleColorsDark();
+	set_imgui_style_win32_like();
+
+	// Workaround for clipboard setup
+	// https://github.com/ocornut/imgui/issues/7455#issuecomment-2027855876
+	auto defaultSetClipboardTextFn = io.SetClipboardTextFn;
+	auto defaultGetClipboardTextFn = io.GetClipboardTextFn;
+	auto defaultClipboardUserData = io.ClipboardUserData;
 
 	ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
 	ImGui_ImplOpenGL3_Init("#version 450");
+
+	io.SetClipboardTextFn = defaultSetClipboardTextFn;
+	io.GetClipboardTextFn = defaultGetClipboardTextFn;
+	io.ClipboardUserData = defaultClipboardUserData;
 }
 
 static void clear_screen() {
