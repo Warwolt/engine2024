0e276d2e48bcaa488bcb7a6a2f122d2a42366b33
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-08-11 04:15:34 +0200

    Add colored logs

--- src/editor/editor_ui.cpp
+++ src/editor/editor_ui.cpp
@@ -95,6 +95,30 @@ namespace editor {
 		shutdown_editor_scene_view(ui.scene_view);
 	}
 
+	static ImU32 log_severity_to_color(plog::Severity severity) {
+		ImU32 color = 0;
+		switch (severity) {
+			case plog::Severity::verbose:
+			case plog::Severity::debug:
+				color = IM_COL32(59, 215, 226, 255);
+				break;
+
+			case plog::Severity::info:
+				color = IM_COL32(255, 255, 255, 255);
+				break;
+
+			case plog::Severity::warning:
+				color = IM_COL32(255, 216, 96, 255);
+				break;
+
+			case plog::Severity::error:
+			case plog::Severity::fatal:
+				color = IM_COL32(255, 0, 0, 255);
+				break;
+		}
+		return color;
+	}
+
 	std::vector<editor::EditorCommand> update_editor_ui(
 		EditorUiState* ui,
 		engine::GameState* game,
@@ -194,19 +218,14 @@ namespace editor {
 			ImGui::ShowDemoWindow(&ui->show_imgui_demo);
 		}
 
-		if (ImGui::Begin("Debug")) {
-			if (ImGui::Button("Log")) {
-				LOG_DEBUG("Hello");
-			}
-		}
-		ImGui::End();
-
 		/* Log Window */
 		if (ImGui::Begin(LOG_WINDOW)) {
 			if (input.log) {
 				// Print log messages
 				for (const platform::LogEntry& entry : *input.log) {
+					ImGui::PushStyleColor(ImGuiCol_Text, log_severity_to_color(entry.severity));
 					ImGui::Text("%s", entry.message.c_str());
+					ImGui::PopStyleColor();
 				}
 
 				// Auto-scroll on new messages unless scroll position is set by user
