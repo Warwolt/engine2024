f0876e725b2969804cafee6198020c58bb981f10
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-08-24 01:50:34 +0200

    frame the problem

--- src/engine/state/scene_graph.h
+++ src/engine/state/scene_graph.h
@@ -2,6 +2,8 @@
 
 #include <core/newtype.h>
 
+#include <glm/vec2.hpp>
+
 #include <vector>
 
 namespace engine {
@@ -21,9 +23,19 @@ namespace engine {
 		Text,
 	};
 
+	// FUCK OH NO SHIT:
+	// No matter what new member we add, if it's big enough we start causing a
+	// bunch of weird crashes.
+	//
+	// This implies that we're _probably_ doing some kind of heap memory stomping
+	// that invalidates other data on the heap. FUCK!
+	//
+	// We need propper tooling to know exactly what is fucking up the heap
+	// memory, otherwise this is practially impossible to figure out. SHIT.
 	struct GraphNode {
 		GraphNodeId id = GraphNodeId(0);
 		NodeType type = NodeType::Root;
+		glm::vec2 position = { 0.0f, 0.0f };
 		std::vector<GraphNode> children;
 	};
 
