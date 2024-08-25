b3424c9f3beea10ac06796c6422327ea85f1a082
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-08-25 16:04:35 +0200

    Add iter tests

--- test/libs/kpeeters/tree_tests.cpp
+++ test/libs/kpeeters/tree_tests.cpp
@@ -10,12 +10,97 @@ TEST(TreeTests, DefaultTree_IsEmpty) {
 	EXPECT_EQ(tree.max_depth(), 0);
 }
 
-TEST(TreeTests, SingleNodeTree) {
+TEST(TreeTests, SingleNodeTree_NonEmpty) {
 	kpeeters::tree<int> tree;
 
-	tree.insert(tree.begin(), 10);
+	// 1
+	tree.insert(tree.begin(), 1);
 
 	EXPECT_FALSE(tree.empty());
 	EXPECT_EQ(tree.size(), 1);
 	EXPECT_EQ(tree.max_depth(), 1);
+}
+
+TEST(TreeTests, NodesIteratedDepthFirst) {
+	kpeeters::tree<int> tree;
+
+	//      1
+	//    /  \
+	//   2    3
+	// / | \
+	// 4 5 6
+	auto root = tree.insert(tree.begin(), 1);
+	auto left_child = tree.append_child(root, 2);
+	tree.append_child(root, 3);
+	tree.append_child(left_child, 4);
+	tree.append_child(left_child, 5);
+	tree.append_child(left_child, 6);
+
+	std::vector<int> nodes;
+	for (int node : tree) {
+		nodes.push_back(node);
+	}
+
+	const std::vector<int> expected = { 1, 2, 4, 5, 6, 3 };
+	EXPECT_EQ(nodes, expected);
+}
+
+TEST(TreeTests, EraseSingleNode) {
+	kpeeters::tree<int> tree;
+
+	//      1
+	//    /  \
+	//   2    3
+	// / | \
+	// 4 5 6
+	auto root = tree.insert(tree.begin(), 1);
+	auto left_child = tree.append_child(root, 2);
+	tree.append_child(root, 3);
+	tree.append_child(left_child, 4);
+	auto node_5 = tree.append_child(left_child, 5);
+	tree.append_child(left_child, 6);
+
+	//      1
+	//    /  \
+	//   2    3
+	// /   \
+	// 4   6
+	tree.erase(node_5);
+
+	std::vector<int> nodes;
+	for (int node : tree) {
+		nodes.push_back(node);
+	}
+
+	const std::vector<int> expected = { 1, 2, 4, 6, 3 };
+	EXPECT_EQ(nodes, expected);
+}
+
+TEST(TreeTests, EraseSubTree) {
+	kpeeters::tree<int> tree;
+
+	//      1
+	//    /  \
+	//   2    3
+	// / | \
+	// 4 5 6
+	auto root = tree.insert(tree.begin(), 1);
+	auto left_child = tree.append_child(root, 2);
+	tree.append_child(root, 3);
+	tree.append_child(left_child, 4);
+	tree.append_child(left_child, 5);
+	tree.append_child(left_child, 6);
+
+	//  1
+	//   \
+	//    3
+	tree.erase(left_child);
+
+	std::vector<int> nodes;
+	for (int node : tree) {
+		nodes.push_back(node);
+	}
+
+	const std::vector<int> expected = { 1, 3 };
+	EXPECT_EQ(nodes, expected);
 }
