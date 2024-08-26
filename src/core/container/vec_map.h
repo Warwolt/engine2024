787f973e81eef1eab528a45f28457163174f6810
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-08-26 23:12:34 +0200

    Initializer list

--- src/core/container/vec_map.h
+++ src/core/container/vec_map.h
@@ -1,21 +1,36 @@
 #pragma once
 
+#include <initializer_list>
 #include <stdint.h>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
 namespace core {
 
-	template <typename K, typename V>
+	template <typename Key, typename T>
 	class VecMap {
 	public:
+		VecMap() = default;
+		VecMap(std::initializer_list<std::pair<const Key, T>> init) {
+			size_t i = 0;
+			for (const auto& [key, value] : init) {
+				m_values.push_back(value);
+				m_key_to_index.emplace(key, i++);
+			}
+		}
+
 		size_t size() const {
-			return 0;
+			return m_values.size();
 		}
 
 		bool empty() const {
-			return true;
+			return m_values.empty();
 		}
 
 	private:
+		std::vector<T> m_values;
+		std::unordered_map<Key, size_t> m_key_to_index;
 	};
 
 } // namespace core
--- test/core/container/vec_map_tests.cpp
+++ test/core/container/vec_map_tests.cpp
@@ -2,9 +2,20 @@
 
 #include <core/container/vec_map.h>
 
-TEST(VecMapTests, InitiallyEmpty) {
+TEST(VecMapTests, DefaultConstructed_IsEmpty) {
 	core::VecMap<std::string, int> vec_map;
 
 	EXPECT_EQ(vec_map.size(), 0);
 	EXPECT_TRUE(vec_map.empty());
+}
+
+TEST(VecMapTests, ConstructedWithInitializerList_IsNonEmpty) {
+	core::VecMap<std::string, int> vec_map = {
+		{ "first", 1 },
+		{ "second", 2 },
+		{ "third", 3 },
+	};
+
+	EXPECT_EQ(vec_map.size(), 3);
+	EXPECT_FALSE(vec_map.empty());
 }
