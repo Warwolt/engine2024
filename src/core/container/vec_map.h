67ddac70d91bf05ac9975256527308589a9427f0
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-08-30 00:04:40 +0200

    Iterator, insert

--- src/core/container/vec_map.h
+++ src/core/container/vec_map.h
@@ -11,12 +11,17 @@ namespace core {
 	template <typename Key, typename T>
 	class VecMap {
 	public:
+		using iterator = std::vector<T>::iterator;
+		using const_iterator = std::vector<T>::const_iterator;
+
 		VecMap() = default;
 		VecMap(std::initializer_list<std::pair<const Key, T>> init) {
 			size_t i = 0;
 			for (const auto& [key, value] : init) {
-				m_values.push_back(value);
-				m_key_to_index.emplace(key, i++);
+				if (!m_key_to_index.contains(key)) {
+					m_values.push_back(value);
+					m_key_to_index.emplace(key, i++);
+				}
 			}
 		}
 
@@ -59,12 +64,48 @@ namespace core {
 			return m_values.back();
 		}
 
+		std::pair<iterator, bool> insert(const std::pair<Key, T>& key_val) {
+			const auto& [key, value] = key_val;
+
+			// update existing key
+			if (m_key_to_index.contains(key)) {
+				size_t index = m_key_to_index[key];
+				iterator it = m_values.begin() + index;
+				*it = value;
+				return { it, false };
+			}
+
+			// new key
+			m_values.push_back(value);
+			size_t index = m_values.size() - 1;
+			m_key_to_index[key] = index;
+			iterator it = m_values.begin() + index;
+
+			return { it, true };
+		}
+
 		T& at(const Key& key) {
 			return m_values[m_key_to_index.at(key)];
 		}
 
 		const T& at(const Key& key) const {
 			return m_values[m_key_to_index.at(key)];
+		}
+
+		iterator begin() {
+			return m_values.begin();
+		}
+
+		iterator end() {
+			return m_values.end();
+		}
+
+		const_iterator begin() const {
+			return m_values.begin();
+		}
+
+		const_iterator end() const {
+			return m_values.end();
 		}
 
 	private:
--- test/core/container/vec_map_tests.cpp
+++ test/core/container/vec_map_tests.cpp
@@ -108,3 +108,96 @@ TEST(VecMapTests, AtOperator_ExistingKey_ReturnsThatValue) {
 	const core::VecMap<std::string, int> const_vec_map = { { "key", 1234 } };
 	EXPECT_EQ(const_vec_map.at("key"), 1234);
 }
+
+TEST(VecMapTests, ConstructedWithInitializerList_DuplicateEntriesIgnored) {
+	core::VecMap<std::string, int> vec_map = {
+		{ "first", 1 },
+		{ "second", 2 },
+		{ "second", 22 },
+	};
+	std::unordered_map<std::string, int> unordered_map = {
+		{ "first", 1 },
+		{ "second", 2 },
+		{ "second", 22 },
+	};
+
+	EXPECT_EQ(vec_map["first"], 1);
+	EXPECT_EQ(vec_map["second"], 2);
+	EXPECT_EQ(unordered_map["first"], 1);
+	EXPECT_EQ(unordered_map["second"], 2);
+	EXPECT_EQ(vec_map.size(), 2);
+}
+
+TEST(VecMapTests, Iterator_IteratesInOrderOfInsertion) {
+	core::VecMap<std::string, int> vec_map = {
+		{ "first", 1 },
+		{ "second", 2 },
+		{ "third", 33 },
+	};
+	const core::VecMap<std::string, int> const_vec_map = {
+		{ "first", 1 },
+		{ "second", 2 },
+		{ "third", 33 },
+	};
+
+	std::vector<int> values;
+	for (int value : vec_map) {
+		values.push_back(value);
+	}
+	std::vector<int> const_values;
+	for (int value : const_vec_map) {
+		const_values.push_back(value);
+	}
+
+	ASSERT_EQ(values.size(), 3);
+	EXPECT_EQ(values[0], 1);
+	EXPECT_EQ(values[1], 2);
+	EXPECT_EQ(values[2], 33);
+
+	ASSERT_EQ(const_values.size(), 3);
+	EXPECT_EQ(const_values[0], 1);
+	EXPECT_EQ(const_values[1], 2);
+	EXPECT_EQ(const_values[2], 33);
+}
+
+TEST(VecMapTests, Insert_NewKey_AddsElementToEnd) {
+	core::VecMap<std::string, int> vec_map {
+		{ "first", 1 },
+	};
+
+	const auto& [iterator, was_inserted] = vec_map.insert({ "second", 22 });
+	std::vector<int> values;
+	for (int value : vec_map) {
+		values.push_back(value);
+	}
+
+	const std::vector<int> expected = { 1, 22 };
+	EXPECT_TRUE(was_inserted);
+	ASSERT_NE(iterator, vec_map.end());
+	EXPECT_EQ(*iterator, 22);
+	EXPECT_EQ(vec_map["first"], 1);
+	EXPECT_EQ(vec_map["second"], 22);
+	EXPECT_EQ(values, expected);
+}
+TEST(VecMapTests, Insert_ExistingKey_OverwritesValue) {
+	core::VecMap<std::string, int> vec_map {
+		{ "first", 1 },
+	};
+
+	const auto& [iterator, was_inserted] = vec_map.insert({ "first", 12 });
+	std::vector<int> values;
+	for (int value : vec_map) {
+		values.push_back(value);
+	}
+
+	const std::vector<int> expected = { 12 };
+	EXPECT_FALSE(was_inserted);
+	ASSERT_NE(iterator, vec_map.end());
+	EXPECT_EQ(*iterator, 12);
+	EXPECT_EQ(vec_map["first"], 12);
+	EXPECT_EQ(values, expected);
+}
+
+// erase
+
+// clear
