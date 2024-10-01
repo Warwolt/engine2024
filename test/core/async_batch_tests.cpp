1b776c3f21d37d2d8fadf677bd2a7e1b8d7830a9
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-10-02 00:18:11 +0200

    tests

---
+++ test/core/async_batch_tests.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+
+#include <core/async_batch.h>
+
+TEST(AsyncBatchTests, DefaultBatch_IsEmptyAndDone) {
+	core::AsyncBatch<int(int)> batch;
+
+	EXPECT_TRUE(batch.is_done());
+	EXPECT_TRUE(batch.values().empty());
+}
+
+TEST(AsyncBatchTests, BatchWithItems_NotUpdated_NotDone) {
+	std::vector<int> items = { 1, 2, 3 };
+	core::AsyncBatch<int(int)> batch(items, [](int x) { return x + 1; });
+
+	EXPECT_FALSE(batch.is_done());
+	EXPECT_TRUE(batch.values().empty());
+}
+
+TEST(AsyncBatchTests, BatchWithItems_UpdatedUntilDone_HasValues) {
+	std::vector<int> items = { 1, 2, 3 };
+	core::AsyncBatch<int(int)> batch(items, [](int x) { return x + 1; });
+
+	while (!batch.is_done()) {
+		batch.update();
+	}
+
+	std::vector<int> expected_values = { 2, 3, 4 };
+	EXPECT_TRUE(batch.is_done());
+	EXPECT_EQ(batch.values(), expected_values);
+}
