b453dd1673ce96d3b851ade4f417bd7f82767e7d
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-10-18 22:01:17 +0200

    Add vector<future<T>> support

--- src/core/future.h
+++ src/core/future.h
@@ -1,16 +1,64 @@
 #pragma once
 
 #include <future>
-#include <optional>
+#include <ranges>
+#include <type_traits>
+#include <vector>
 
-namespace core::future {
+namespace core {
 
 	template <typename T>
-	bool has_value(const std::future<T>& future) {
+	bool future_is_ready(const std::future<T>& future) {
 		if (future.valid()) {
 			return future.wait_for(std::chrono::seconds(0)) == std::future_status::ready;
 		}
 		return false;
 	}
 
-} // namespace core::future
+	template <typename Iter, typename F>
+	auto batch_async(std::launch policy, Iter first, Iter last, F&& fn) {
+		using result_type = typename std::invoke_result<F, decltype(*first)>::type;
+		std::vector<std::future<result_type>> batch;
+
+		for (Iter it = first; it != last; ++it) {
+			batch.push_back(std::async(policy, std::forward<F>(fn), *it));
+		}
+
+		return batch;
+	}
+
+	template <std::ranges::range R, typename F>
+	auto batch_async(std::launch policy, R&& range, F&& fn) {
+		return batch_async(policy, std::begin(range), std::end(range), fn);
+	}
+
+	template <typename T, typename OutputIt>
+	void get_all_batch_values(std::vector<std::future<T>>& batch, OutputIt out_first) {
+		for (std::future<T>& future : batch) {
+			*out_first = future.get();
+			++out_first;
+		}
+	}
+
+	template <typename T, typename OutputIt>
+	void get_ready_batch_values(std::vector<std::future<T>>& batch, OutputIt out_first) {
+		for (std::future<T>& future : batch) {
+			if (future_is_ready(future)) {
+				*out_first = future.get();
+				++out_first;
+			}
+		}
+	}
+
+	template <typename T>
+	std::vector<T> get_ready_batch_values(std::vector<std::future<T>>& batch) {
+		std::vector<T> values;
+		for (std::future<T>& future : batch) {
+			if (future_is_ready(future)) {
+				values.push_back(future.get());
+			}
+		}
+		return values;
+	}
+
+} // namespace core
