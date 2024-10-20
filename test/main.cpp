3ab4d0cd34d4c7d5f90d88951528399c315cedb2
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-10-20 17:24:11 +0200

    init platform

--- test/main.cpp
+++ test/main.cpp
@@ -20,18 +20,16 @@ namespace testing {
 int main(int argc, char** argv) {
 	::testing::InitGoogleTest(&argc, argv);
 
-	printf("DEBUG: Hello world!\n");
-
-	// platform::init_test_logging();
-	// ASSERT(platform::initialize(), "platform::initialize failed");
-	// platform::Window window = core::unwrap(platform::Window::create(0, 0, SDL_WINDOW_HIDDEN, "Unit Testing"), [] {
-	// 	ABORT("platform::create_window failed");
-	// });
-	// SDL_GLContext sdl_gl_context = core::unwrap(platform::create_gl_context(window.sdl_window()), [](platform::CreateGLContextError error) {
-	// 	ABORT("platform::create_gl_context() returned %s", core::util::enum_to_string(error));
-	// });
-	// platform::OpenGLContext gl_context = platform::OpenGLContext(sdl_gl_context);
-	// g_gl_context_ptr = &gl_context;
+	platform::init_test_logging();
+	ASSERT(platform::initialize(), "platform::initialize failed");
+	platform::Window window = core::unwrap(platform::Window::create(0, 0, SDL_WINDOW_HIDDEN, "Unit Testing"), [] {
+		ABORT("platform::create_window failed");
+	});
+	SDL_GLContext sdl_gl_context = core::unwrap(platform::create_gl_context(window.sdl_window()), [](platform::CreateGLContextError error) {
+		ABORT("platform::create_gl_context() returned %s", core::util::enum_to_string(error));
+	});
+	platform::OpenGLContext gl_context = platform::OpenGLContext(sdl_gl_context);
+	g_gl_context_ptr = &gl_context;
 
 	// int error_code = RUN_ALL_TESTS();
 
