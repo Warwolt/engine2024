4ff9bc8850d829bf3c91b2a01d891e0de9d198ad
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-10-20 02:44:06 +0200

    load image and font test

--- test/platform/resource_manager_tests.cpp
+++ test/platform/resource_manager_tests.cpp
@@ -12,22 +12,30 @@ TEST(ResourceManagerTests, InitiallyEmpty) {
 
 TEST(ResourceManagerTests, LoadManifest_WithExistingFiles_AreLoadedIntoManager) {
 	std::filesystem::path working_directory = std::filesystem::current_path();
-	platform::ImageDeclaration image_decl = {
-		.name = "TestImage",
-		.path = working_directory / "test/platform/test_data/test_image.png",
-	};
-
 	platform::ResourceManager resource_manager;
 	platform::ResourceManifest manifest = {
-		.images = { image_decl }
+		.fonts = { platform::FontDeclaration {
+			.name = "test_font",
+			.path = working_directory / "test/platform/test_data/test_font.ttf",
+			.size = 16,
+		} },
+		.images = { platform::ImageDeclaration {
+			.name = "test_image",
+			.path = working_directory / "test/platform/test_data/test_image.png",
+		} }
 	};
-	std::shared_ptr<const platform::ResourceLoadProgress> progress = resource_manager.load_manifest(manifest);
 
+	std::shared_ptr<const platform::ResourceLoadProgress> progress = resource_manager.load_manifest(manifest);
 	WAIT_FOR(progress->is_done(), std::chrono::seconds(1)) {
 		resource_manager.update(testing::gl_context());
 	}
 
-	EXPECT_TRUE(resource_manager.textures().contains("TestImage"));
+	ASSERT_TRUE(resource_manager.fonts().contains("test_font"));
+	EXPECT_EQ(resource_manager.fonts().at("test_font").size, 16);
+
+	ASSERT_TRUE(resource_manager.textures().contains("test_image"));
+	EXPECT_EQ(resource_manager.textures().at("test_image").size.x, 16);
+	EXPECT_EQ(resource_manager.textures().at("test_image").size.y, 16);
 }
 
 // TEST(ResourceManagerTests, LoadManifest_WithBadPaths_NotLoadedIntoManager) {
