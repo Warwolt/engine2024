de4cccfbbe28e6ab8afaa202c106b3850f045f4e
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-10-20 17:42:02 +0200

    Init window

--- test/main.cpp
+++ test/main.cpp
@@ -22,9 +22,9 @@ int main(int argc, char** argv) {
 
 	platform::init_test_logging();
 	ASSERT(platform::initialize(), "platform::initialize failed");
-	// platform::Window window = core::unwrap(platform::Window::create(0, 0, SDL_WINDOW_HIDDEN, "Unit Testing"), [] {
-	// 	ABORT("platform::create_window failed");
-	// });
+	platform::Window window = core::unwrap(platform::Window::create(0, 0, SDL_WINDOW_HIDDEN, "Unit Testing"), [] {
+		ABORT("platform::create_window failed");
+	});
 	// SDL_GLContext sdl_gl_context = core::unwrap(platform::create_gl_context(window.sdl_window()), [](platform::CreateGLContextError error) {
 	// 	ABORT("platform::create_gl_context() returned %s", core::util::enum_to_string(error));
 	// });
