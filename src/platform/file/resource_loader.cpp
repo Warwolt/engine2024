09ace8e0299a441a657498a8e2bbcace67ba24a1
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2024-10-23 09:44:18 +0200

    Remove superflous namespacing

--- src/platform/file/resource_loader.cpp
+++ src/platform/file/resource_loader.cpp
@@ -5,10 +5,10 @@
 
 namespace platform {
 
-	std::expected<platform::FontAtlas, ResourceLoadError> ResourceFileIO::load_font(std::filesystem::path font_path, uint8_t font_size) {
-		std::expected<platform::FontFace, std::string> font_face = platform::load_font_face(font_path);
+	std::expected<FontAtlas, ResourceLoadError> ResourceFileIO::load_font(std::filesystem::path font_path, uint8_t font_size) {
+		std::expected<FontFace, std::string> font_face = load_font_face(font_path);
 		if (font_face.has_value()) {
-			return platform::generate_font_atlas(font_face.value(), font_size);
+			return generate_font_atlas(font_face.value(), font_size);
 		}
 		else {
 			std::string error_msg = std::format(
@@ -21,8 +21,8 @@ namespace platform {
 		}
 	}
 
-	std::expected<platform::Image, ResourceLoadError> ResourceFileIO::load_image(std::filesystem::path image_path) {
-		std::expected<platform::Image, std::string> image = platform::read_image(image_path);
+	std::expected<Image, ResourceLoadError> ResourceFileIO::load_image(std::filesystem::path image_path) {
+		std::expected<Image, std::string> image = read_image(image_path);
 		if (image.has_value()) {
 			return std::move(image.value());
 		}
@@ -48,14 +48,14 @@ namespace platform {
 
 		m_jobs.push_back(ResourceLoadJob {
 			.font_batch = core::batch_async(std::launch::async, manifest.fonts, [file_io = m_file_io](const FontDeclaration& font_decl) -> LoadFontResult {
-				std::expected<platform::FontAtlas, ResourceLoadError> result = file_io->load_font(font_decl.path, font_decl.size);
+				std::expected<FontAtlas, ResourceLoadError> result = file_io->load_font(font_decl.path, font_decl.size);
 				if (result.has_value()) {
 					return NamedFontAtlas { .name = font_decl.name, .atlas = result.value() };
 				}
 				return std::unexpected(result.error());
 			}),
 			.image_batch = core::batch_async(std::launch::async, manifest.images, [file_io = m_file_io](const ImageDeclaration& image_decl) -> LoadImageResult {
-				std::expected<platform::Image, ResourceLoadError> result = file_io->load_image(image_decl.path);
+				std::expected<Image, ResourceLoadError> result = file_io->load_image(image_decl.path);
 				if (result.has_value()) {
 					return NamedImage { .name = image_decl.name, .image = std::move(result.value()) };
 				}
@@ -67,7 +67,7 @@ namespace platform {
 		return progress;
 	}
 
-	void ResourceLoader::update(platform::OpenGLContext* gl_context) {
+	void ResourceLoader::update(OpenGLContext* gl_context) {
 		for (ResourceLoadJob& job : m_jobs) {
 			_process_fonts(&job.font_batch, &job.payload->fonts, gl_context, job.payload.get());
 			_process_images(&job.image_batch, &job.payload->textures, gl_context, job.payload.get());
@@ -76,14 +76,14 @@ namespace platform {
 
 	void ResourceLoader::_process_fonts(
 		std::vector<std::future<LoadFontResult>>* font_batch,
-		core::vector_map<std::string, platform::Font>* fonts,
-		platform::OpenGLContext* gl_context,
+		core::vector_map<std::string, Font>* fonts,
+		OpenGLContext* gl_context,
 		ResourceLoadPayload* payload
 	) {
 		for (const LoadFontResult& result : core::get_ready_batch_values(*font_batch)) {
 			if (result.has_value()) {
 				const auto& [name, atlas] = result.value();
-				fonts->insert({ name, platform::create_font_from_atlas(gl_context, atlas) });
+				fonts->insert({ name, create_font_from_atlas(gl_context, atlas) });
 			}
 			else {
 				auto& [error_msg, invalid_path] = result.error();
@@ -95,14 +95,14 @@ namespace platform {
 
 	void ResourceLoader::_process_images(
 		std::vector<std::future<LoadImageResult>>* image_batch,
-		core::vector_map<std::string, platform::Texture>* textures,
-		platform::OpenGLContext* gl_context,
+		core::vector_map<std::string, Texture>* textures,
+		OpenGLContext* gl_context,
 		ResourceLoadPayload* payload
 	) {
 		for (const LoadImageResult& result : core::get_ready_batch_values(*image_batch)) {
 			if (result.has_value()) {
 				const auto& [name, image] = result.value();
-				platform::Texture texture = gl_context->add_texture(image.data.get(), image.width, image.height);
+				Texture texture = gl_context->add_texture(image.data.get(), image.width, image.height);
 				textures->insert({ name, texture });
 			}
 			else {
